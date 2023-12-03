b24b2486440133897e55be2c239156ae98abcc83
Bastian Eicher <bastian@eicher.net>
2023-12-03 17:19:47 +0100

    Use top-level statements

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -1,16 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Sample;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
-}
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Sample.Startup>()).Build().Run();
